3ee4a72d05690ec51427f3a911850e2e9cdc46da
oedev-apps <moettinger@gmail.com>
2023-10-01 22:54:04 +0000

    wired up component updates

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,9 @@
 <template>
   <q-page class="flex flex-center" :style-fn="setOffset">
-    <!-- <div>
-      <img src="sample.png">
-    </div> -->
-    <div ref="pixiContainer" class="pixi-canvas-container" style="border: 1px solid greenyellow;">
-    </div>
+    <div ref="pixiContainer" class="pixi-canvas-container" style="border: 1px solid greenyellow;"></div>
+    <q-card flat class="absolute-bottom q-mb-sm q-mx-sm q-px-xs row text-white" style="background-color: rgba(0, 0, 0, 0);">
+      <div class="col">FPS: {{ fps }}</div> <div class="col">Elapsed: {{ elapsedSec }}s</div>
+    </q-card>
   </q-page>
 </template>
 
@@ -18,6 +17,8 @@ const sprite = PIXI.Sprite.from('sample.png');
 sprite.anchor.set(0.5);
 sprite.x = app.screen.width / 2;
 sprite.y = app.screen.height / 2;
+sprite.scale.x = 0.25;
+sprite.scale.y = 0.25;
 let elapsed = 0.0;
 let uiUpdate = 1;
 document.body.style.backgroundColor = 'black';
@@ -26,21 +27,27 @@ export default defineComponent({
   name: 'IndexPage',
   data () {
     return ({
-      fps: 0
+      fps: 0,
+      elapsedSec: 0
     })
   },
   mounted () {
+    // Init Pixi canvas
     const pixiEl = this.$refs.pixiContainer;
-    // console.log(pixiEl);
     pixiEl.appendChild(app.view);
     app.stage.addChild(sprite);
+    // Update function
     app.ticker.add((delta) => {
-      elapsed += delta;
-      if (elapsed >= uiUpdate) {
-
+      // Track elapsed time
+      elapsed += app.ticker.deltaMS;
+      // Update Vue components
+      if (elapsed / 1000 >= uiUpdate) {
+        this.fps = app.ticker.FPS.toFixed(2);
+        this.elapsedSec = Math.round(elapsed / 1000);
         uiUpdate++;
       }
-      // sprite.y = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+      // Move the Sprite
+      sprite.y = Math.max(sprite.y - 1, 0);
     });
   },
   methods: {
